1e9e9d74015ef7ba5ad6687d60d42a20eac996e6
akashpj123 <akashpj77@gmail.com>
2024-06-28 14:51:18 +0530

    file changes

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: './.env' });
+import crypto from 'crypto';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import Razorpay from 'razorpay';
 
 // Import your routers
 import { adminmovie } from './routes/admin-movie.js';
@@ -16,11 +18,13 @@ import { paymentRoutes } from './routes/payment.js';
 
 const app = express();
 
-// Middleware setup
+// Use middleware
 app.use(cors({
+  
     origin: ["https://movie-villa-chi.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
+  
 }));
 app.use(logger('dev'));
 app.use(express.json());
@@ -35,21 +39,43 @@ app.use('/api/payment', paymentRoutes);
 app.use('/booking', booking);
 app.use('/bookings', bookings);
 
+// MongoDB connection
+
+// MongoDB connection
 // MongoDB connection using Mongoose
 const uri = process.env.MONGODB_URI || "mongodb+srv://movie:akash123@movie-villa.xmqepbp.mongodb.net/movie-villa?retryWrites=true&w=majority&appName=movie-villa";
 
 mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000 // Timeout after 5s instead of waiting indefinitely
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000 // Timeout after 5s instead of waiting indefinitely
 })
 .then(() => console.log('MongoDB connected...'))
-.catch(err => console.log('MongoDB connection error:', err));
+.catch(err => console.log(err));
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
 
 // Start server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`);
 });
 
 export default app;
